cb53cf3476c6e8e3440c7f2b2bf57c3c1b4e1bda
jephthah-orobia <jephthah.orobia@gmail.com>
2025-01-07 13:11:38 -0600

    implement task

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,17 @@ using namespace std;
 
 int main()
 {
+  float firstVar;
+  float& secondVar = firstVar;
 
-  cout << "Hello World!" << endl;
+  secondVar = 3.14159265358979323846;
 
+  cout << "\033[4mFirst variable\033[0m" << endl
+       << "\t-address: " << &firstVar << endl
+       << "\t-value: " << firstVar << endl
+       << "\033[4mSecond variable\033[0m" << endl
+       << "\t-address: " << &secondVar << endl
+       << "\t-value: " << secondVar << endl << endl;
   // ********************** DO NOT CHANGE **********************
   // Print a new line and ask user for any key before exiting
   // ***********************************************************
